ba0808f177fdf2f2f1496d618d48ae37696187e8
Volodymyr Andriukov <77066771+vandrukov@users.noreply.github.com>
2023-03-09 19:15:03 +0100

    Create stage_3-6_That_is_the_question.sh

---
+++ stage_3-6_That_is_the_question.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Welcome to the True or False Game!"
+curl --silent --output ID_card.txt http://127.0.0.1:8000/download/file.txt
+read line < ID_card.txt
+username=$(echo $line | cut -d '"' -f 4 )
+password=$(echo $line | cut -d '"' -f 8 )
+message=$(curl --silent --cookie-jar cookie.txt --user $username:$password http://127.0.0.1:8000/login)
+echo "Login message: $message"
+answer=$(curl --silent --cookie cookie.txt http://127.0.0.1:8000/game)
+echo "Response: $answer"
